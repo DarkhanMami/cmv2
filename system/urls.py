a01699daffb5679076c807dbfbfd3be246e8c91b
DarkhanMami <dmami@nu.edu.kz>
2019-10-17 17:23:20 +0500

    vmb all tags

--- system/urls.py
+++ system/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import include, path
+import all_tags.Moldabek.all_tags
 
 admin.site.site_header = 'Администрирование'
 admin.site.site_title = 'CM_v2'
@@ -11,6 +12,8 @@ urlpatterns = [
     # path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('tinymce/', include('tinymce.urls')),
+    path('update_VMB_tags/', all_tags.Moldabek.all_tags),
+
 ]
 
 urlpatterns += i18n_patterns(
