423be74518da46ee35cadc04471c14a3b11f657d
DarkhanMami <dmami@nu.edu.kz>
2019-11-20 18:23:25 +0600

    imbalance

--- api/views.py
+++ api/views.py
@@ -755,7 +755,7 @@ def update_imbalance(request):
 
             cur.execute("SELECT id FROM stations where code='" + well.name + "' limit 1")
             row_values = cur.fetchone()
-            station_id = row_values[0]
+            station_id = int(row_values[0])
 
             cur.execute("SELECT * FROM fc_data_last where reg=30005 and station_id=" + station_id
                         + " order by save_time limit 1")
