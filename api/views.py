bb43a8487a58ce6a749e91b425a0aee259d770e9
DarkhanMami <dmami@nu.edu.kz>
2019-11-20 18:25:53 +0600

    imbalance

--- api/views.py
+++ api/views.py
@@ -755,15 +755,18 @@ def update_imbalance(request):
 
             cur.execute("SELECT id FROM stations where code='" + well.name + "' limit 1")
             row_values = cur.fetchone()
+            print(row_values)
             station_id = int(row_values[0])
 
             cur.execute("SELECT * FROM fc_data_last where reg=30005 and station_id=" + station_id
                         + " order by save_time limit 1")
             row_values = cur.fetchone()
+            print(row_values)
             imb.imbalance = float(row_values[2])
             imb.timestamp = datetime.strptime(row_values[3], '%Y_%m_%d %H:%M:%S')
             cur.execute("SELECT avg_1997 FROM daily_data where station_id=" + station_id + " order by day limit 1")
             row_values = cur.fetchone()
+            print(row_values)
             imb.avg_1997 = float(row_values[0])
             imb.save()
 
