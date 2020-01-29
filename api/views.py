1e3d34a883e1b2ea60ba33565ffa17f013d918ee
madinurpan <40703510+madinurpan@users.noreply.github.com>
2020-01-29 21:21:49 +0600

    get

--- api/views.py
+++ api/views.py
@@ -774,7 +774,9 @@ def update_imbalance(request):
             cur.execute("SELECT * FROM fc_data_last where reg=30005 and station_id=" + str(station_id) + " order by savetime desc")
             row_values = cur.fetchone()
             try:
-                imb = models.Imbalance.objects.get(well=well)
+                imb = models.Imbalance.objects.get(well=well.pk)
+                imb_history = models.ImbalanceHistory.objects.create(imb=imb,well=imb.well,imbalance=imb.imbalance,avg_1997=imb.avg_1997,timestamp=imb.timestamp)
+                imb_history.save()
             except:
                 imb = models.Imbalance.objects.create(well=well)
             try:
