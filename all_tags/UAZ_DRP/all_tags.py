457d9fe29d3ca3e53e8f6f68e2896d56141e8bc8
DarkhanMami <dmami@nu.edu.kz>
2021-01-05 23:58:04 +0600

    new uaz drp tags

--- all_tags/UAZ_DRP/all_tags.py
+++ all_tags/UAZ_DRP/all_tags.py
@@ -22,39 +22,43 @@ try:
                 'РГС_4_Уровень', 'РГС_4_Уровень_подтоварной_жидкости', 'РГС_1_Температура', 'РГС_2_Температура',
                 'РГС_3_Температура', 'РГС_4_Температура', 'РГС_1_Обьем', 'РГС_2_Обьем', 'РГС_3_Обьем', 'РГС_4_Обьем']
 
-    Data2 = opc.read(rgs_tags, group='test2', timeout=100000)
+    t = []
+    for tt in rgs_tags:
+        t.append(tt.decode('utf-8'))
+
+    Data2 = opc.read(t, group='test2', timeout=100000)
     opc.remove('test')
     opc.remove('test2')
 
     for i in Data2:
         tag = i[0]
-        if tag.decode('utf-8') == 'РГС_1_Уровень':
+        if tag == 'РГС_1_Уровень':
             tag = 'H1_UAZ'
-        if tag.decode('utf-8') == 'РГС_2_Уровень':
+        if tag == 'РГС_2_Уровень':
             tag = 'H2_UAZ'
-        if tag.decode('utf-8') == 'РГС_3_Уровень':
+        if tag == 'РГС_3_Уровень':
             tag = 'H3_UAZ'
-        if tag.decode('utf-8') == 'РГС_3_Уровень_подтоварной_жидкости':
+        if tag == 'РГС_3_Уровень_подтоварной_жидкости':
             tag = 'H3w_UAZ'
-        if tag.decode('utf-8') == 'РГС_4_Уровень':
+        if tag == 'РГС_4_Уровень':
             tag = 'H4_UAZ'
-        if tag.decode('utf-8') == 'РГС_4_Уровень_подтоварной_жидкости':
+        if tag == 'РГС_4_Уровень_подтоварной_жидкости':
             tag = 'H4w_UAZ'
-        if tag.decode('utf-8') == 'РГС_1_Температура':
+        if tag == 'РГС_1_Температура':
             tag = 'T1_UAZ'
-        if tag.decode('utf-8') == 'РГС_2_Температура':
+        if tag == 'РГС_2_Температура':
             tag = 'T2_UAZ'
-        if tag.decode('utf-8') == 'РГС_3_Температура':
+        if tag == 'РГС_3_Температура':
             tag = 'T3_UAZ'
-        if tag.decode('utf-8') == 'РГС_4_Температура':
+        if tag == 'РГС_4_Температура':
             tag = 'T4_UAZ'
-        if tag.decode('utf-8') == 'РГС_1_Обьем':
+        if tag == 'РГС_1_Обьем':
             tag = 'V1_UAZ'
-        if tag.decode('utf-8') == 'РГС_2_Обьем':
+        if tag == 'РГС_2_Обьем':
             tag = 'V2_UAZ'
-        if tag.decode('utf-8') == 'РГС_3_Обьем':
+        if tag == 'РГС_3_Обьем':
             tag = 'V3_UAZ'
-        if tag.decode('utf-8') == 'РГС_4_Обьем':
+        if tag == 'РГС_4_Обьем':
             tag = 'V4_UAZ'
 
         value = i[1]
